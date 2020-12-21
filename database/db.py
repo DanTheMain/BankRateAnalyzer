fa8be6dc5d5e62aae79c7908dffa477b8dd02af4
DanTheMain <1dan.da.1man@gmail.com>
2020-12-21 21:04:55 +0300

    removed pandas, added date formatting, db read in progress

--- database/db.py
+++ database/db.py
@@ -1,1 +1,29 @@
+from psycopg2_wrapper.database import Database
+import json
 
+
+class DBParser:
+
+    def __init__(self, **kwargs):
+        self._db = Database(host=kwargs['host_name'],
+                            database=kwargs['db_name'],
+                            user=kwargs['db_user'],
+                            password=kwargs['db_password'])
+        if kwargs.get('export_filepath'):
+            self._export_db_file = kwargs['export_filepath']
+        self._bank_documents_table_name = kwargs.get('bank_documents_table') or 'banks_documents'
+        self._bank_name_to_id_table_name = kwargs.get('bank_ids_table') or 'bank_ids'
+
+    def get_bank_documents(self, bank_id: int):
+        result = self._db.execute_query(f'select * from {self._bank_documents_table_name} where id={bank_id}')
+        return dict(result)
+
+    def _get_bank_id(self, bank_name: str) -> int:
+        result = self._db.execute_query(f'select ID from {self._bank_name_to_id_table_name} where name={bank_name}')
+        return result if result else -1
+
+    def add_bank(self, bank_name: str) -> int:
+        pass  # TODO - insert into banks
+
+    def check_bank(self, bank_name: str) -> bool:
+        pass  # TODO: check for record id
--- parsers/parser_bankiru.py
+++ parsers/parser_bankiru.py
@@ -1,12 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 from time import sleep
 from typing import Optional
 import config
-
-
-config.BANK_NAME
+import datetime
 
 
 class PageParser:
@@ -14,22 +11,30 @@ class PageParser:
     def __init__(self):
         pass
 
-    def _parse_table(self, item):
-        res = pd.DataFrame()
-        message_text = item.find('div',{'class': 'responses__item__message markup-inside-small markup-inside-small--bullet'})
-        rating = item.find('span',{'data-test': 'responses-rating-grade'})
-        date_time = item.find('time', {'data-test': 'responses-datetime'})
-        res=res.append(pd.DataFrame([[message_text.text if message_text else 'no message', rating.text if rating else 'no rating', date_time.text]], columns = ['message_text','rating', 'date_time']), ignore_index=True)
+    @staticmethod
+    def _parse_item(item):
+        message_text = item.find('div',
+                                 {'class': 'responses__item__message markup-inside-small markup-inside-small--bullet'})
+        rating = item.find('span',
+                           {'data-test': 'responses-rating-grade'})
+        date_time_str = item.find('time',
+                                  {'data-test': 'responses-datetime'})
+        date_time_str = date_time_str.text.strip() or date_time_str
+        date_time = None
+        try:
+            date_time = datetime.datetime.strptime(date_time_str, '%d.%m.%Y %H:%M')
+        except ValueError as e:
+            print(f"Failed to get date time from {date_time_str}! Details: {e}")
+        date_time_str = date_time.date().strftime('%d.%m.%Y') if date_time else date_time_str
+        res = {'text': message_text.text.strip() if message_text else 'no message',
+               'rating': int(rating.text.strip()) if rating else -1,
+               'date': date_time_str}
         return res
 
     def find_item_match_in_source(self, item_data, source):
-        result = pd.DataFrame()
-        soup = BeautifulSoup(source.text, features="html.parser") 
-        tables=soup.find_all(item_data['item_tag'], item_data['search_match_tags'])
-        for item in tables:
-            res=self._parse_table(item)
-            result = result.append(res)
-        return result
+        soup = BeautifulSoup(source.text, features="html.parser")
+        return [self._parse_item(item) for item in
+                soup.find_all(item_data['item_tag'], item_data['search_match_tags'])]
 
 
 class PageSourceRetriever:
@@ -40,7 +45,7 @@ class PageSourceRetriever:
             self._session.headers.update(header_data)
         self._default_retry_wait_sec = 1
     
-    def get_source(self, url):
+    def get_source(self, url: str):
         try:
             return self._session.get(url)
         except Exception as e:
@@ -51,35 +56,30 @@ class PageSourceRetriever:
 
 class BankRatingParser:
 
-    def __init__(self, name: str, page_num_limit: int, results_file: Optional[str] = None):
+    def __init__(self, name: str, page_num_limit: int):
         self._bank_name = name
-        self._page_rage = range(1, page_num_limit + 1)
-        if results_file:
-            self._results_file = results_file
+        self._page_range = range(1, page_num_limit + 1)
         self._rate_source_url = config.BANKIRU_URL
         self._header_data = {'user-agent': 'Mozilla/5.1', 'accept': '*/*'}
         self._rating_item_search_data = {'item_tag': 'article', 'search_match_tags': {'class': 'responses__item'}}
         
     def get_rating_data(self, page_parse_wait_sec: int = 0.1):
-            result = pd.DataFrame()
+        p_parser = PageParser()
+        sp_retriever = PageSourceRetriever(header_data = self._header_data)
 
-            p_parser = PageParser()
-            sp_retriever = PageSourceRetriever(header_data = self._header_data)
+        result = list()
+        for i in self._page_range:
+            res = p_parser.find_item_match_in_source(
+                source=sp_retriever.get_source(self._rate_source_url + self._bank_name + f'?page={i}'),
+                item_data=self._rating_item_search_data)
+            sleep(page_parse_wait_sec)
+            result += res
 
-            for i in self._page_rage:
-                res = p_parser.find_item_match_in_source(
-                    source=sp_retriever.get_source(self._rate_source_url + self._bank_name + f'?page={i}'),
-                    item_data=self._rating_item_search_data)
-                sleep(page_parse_wait_sec)
-                result=result.append(res, ignore_index=True)
+        return result
 
-            if self._results_file:
-                result.to_excel(self._results_file)
-            
-            return result
-        
-        
 
 if __name__ == "__main__":
-    brp = BankRatingParser(config.bank_name, 1, 'results.xlsx')
+    brp = BankRatingParser(config.BANK_NAME, 4)
     ratings_records = brp.get_rating_data()
+    print(len(ratings_records))
+    [print(record) for record in list(ratings_records)]
